e3c49750ad6885eb4c318532bf7b49861cc5e047
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-20 02:20:37 +0000

    aws-edb-postgres-tuner-dbt2 - save results to a dedicated directory

--- aws-edb-postgres-tuner-dbt2/04_push_results/run.sh
+++ aws-edb-postgres-tuner-dbt2/04_push_results/run.sh
@@ -1,21 +1,6 @@
 #!/bin/bash -eux
 # Push DBT2 files to the S3 bucket
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-# Extract the archive containing the report and data
-tar xzf "${SCRIPT_DIR}/../03_execute/tuner-off/tuner-off.tar.gz" -C "${SCRIPT_DIR}"
-tar xzf "${SCRIPT_DIR}/../03_execute/tuner-measurement/tuner-measurement.tar.gz" -C "${SCRIPT_DIR}"
-tar xzf "${SCRIPT_DIR}/../03_execute/tuner-final/tuner-final.tar.gz" -C "${SCRIPT_DIR}"
-mkdir dbt2-data
-mv "${SCRIPT_DIR}/tmp/tuner-off" "${SCRIPT_DIR}/dbt2-data/"
-mv "${SCRIPT_DIR}/tmp/tuner-measurement" "${SCRIPT_DIR}/dbt2-data/"
-mv "${SCRIPT_DIR}/tmp/tuner-final" "${SCRIPT_DIR}/dbt2-data/"
-rm -rf "${SCRIPT_DIR}/tmp"
-# Copy infrastructure.yml and vars.yml
-cp "${SCRIPT_DIR}/../infrastructure.yml" "${SCRIPT_DIR}/dbt2-data/"
-cp "${SCRIPT_DIR}/../vars.yml" "${SCRIPT_DIR}/dbt2-data/"
-cd "${SCRIPT_DIR}/dbt2-data"
+cd "${RESULTS_DIRECTORY}/dbt2-data"
 date=$(date +'%Y-%m-%dT%H:%M:%S')
 
 aws s3 cp infrastructure.yml "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/"
