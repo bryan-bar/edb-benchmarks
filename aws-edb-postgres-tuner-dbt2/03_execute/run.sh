26897fcf7730df57e15d239d08ed3818a87e74d8
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-27 20:21:11 +0000

    aws-edb-postgres-tuner-dbt2 - terraform project saved into results directory

--- aws-edb-postgres-tuner-dbt2/03_execute/run.sh
+++ aws-edb-postgres-tuner-dbt2/03_execute/run.sh
@@ -7,6 +7,7 @@ export ANSIBLE_PIPELINING=true
 export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
+TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 
 # Run the benchmark
 ansible-playbook \
