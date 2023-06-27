d090b2a205e36e27612cd988f63c57c260f46fad
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-27 20:23:04 +0000

    aws-dbt2-aurora - terraform project saved into results directory

--- aws-dbt2-aurora/05_push_results/run.sh
+++ aws-dbt2-aurora/05_push_results/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -eux
 # Push DBT2 files to the S3 bucket
+
+TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 cd $RESULTS_DIRECTORY/dbt2-data
 date=$(date +'%Y-%m-%dT%H:%M:%S')
 
+aws s3 cp "${TERRAFORM_PROJECT_PATH}/" "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/${TERRAFORM_PROJECT_NAME}/" --recursive
 aws s3 cp readme.txt s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/
 aws s3 cp infrastructure.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/
 aws s3 cp vars.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/
