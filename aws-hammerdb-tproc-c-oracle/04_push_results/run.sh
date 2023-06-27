00648089cc0139b0ab3fe6d406af6e3f5a93d71d
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-27 20:24:14 +0000

    aws-hammerdb-tproc-c-oracle - terraform project saved into results directory

--- aws-hammerdb-tproc-c-oracle/04_push_results/run.sh
+++ aws-hammerdb-tproc-c-oracle/04_push_results/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 # Push HammerDB files to the S3 bucket
+TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 cd "${RESULTS_DIRECTORY}/tprocc-data"
 date=$(date +'%Y-%m-%dT%H:%M:%S')
 
+aws s3 cp "${TERRAFORM_PROJECT_PATH}/" "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/${TERRAFORM_PROJECT_NAME}/" --recursive
 aws s3 cp ./ "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/" --recursive
