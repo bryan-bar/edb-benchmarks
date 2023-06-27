ba9d5aa264da2f0ec8dcf08c4faaff2af805841f
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-27 20:24:44 +0000

    aws-pgbench-postgresql - terraform project saved into results directory

--- aws-pgbench-postgresql/05_push_results/run.sh
+++ aws-pgbench-postgresql/05_push_results/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -eux
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Push pgbench files to the S3 bucket
+TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 cd "$RESULTS_DIRECTORY/report-data"
 date=$(date +'%Y-%m-%dT%H:%M:%S')
 
+aws s3 cp "${TERRAFORM_PROJECT_PATH}/" "s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/${TERRAFORM_PROJECT_NAME}/" --recursive
 aws s3 cp ${RESULTS_DIRECTORY}/report-data/infrastructure.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/infrastructure.yml
 aws s3 cp ${RESULTS_DIRECTORY}/report-data/vars.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/vars.yml
 aws s3 cp ${RESULTS_DIRECTORY}/report-data/ s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/report-data --recursive
