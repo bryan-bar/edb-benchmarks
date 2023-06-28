ecef5a7856e42cfbb871caf584f1a6ed7beeadce
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-28 04:05:55 +0000

    aws-edb-postgres-tuner-dbt2 - handle benchmark results within the playbook

--- aws-edb-postgres-tuner-dbt2/03_execute/run.sh
+++ aws-edb-postgres-tuner-dbt2/03_execute/run.sh
@@ -19,18 +19,9 @@ ansible-playbook \
 	-e "dbt2_warehouse=${DBT2_WAREHOUSE}" \
 	-e "dbt2_connections=${DBT2_CONNECTIONS}" \
 	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
-	"${SCRIPT_DIR}/playbook-dbt2-run.yml"
+	-e "results_directory=${RESULTS_DIRECTORY}/dbt2-data" \
+	./playbook-dbt2-run.yml
 
-# Move data to results directory
-[[ ! -d "$RESULTS_DIRECTORY/dbt2-data" ]] && mkdir -p "$RESULTS_DIRECTORY/dbt2-data"
-# Extract the archive containing the report and data
-tar xzf "${SCRIPT_DIR}/tuner-off/tuner-off.tar.gz" -C "${SCRIPT_DIR}"
-tar xzf "${SCRIPT_DIR}/tuner-measurement/tuner-measurement.tar.gz" -C "${SCRIPT_DIR}"
-tar xzf "${SCRIPT_DIR}/tuner-final/tuner-final.tar.gz" -C "${SCRIPT_DIR}"
-mv "${SCRIPT_DIR}/tmp/tuner-off" "${RESULTS_DIRECTORY}/dbt2-data/"
-mv "${SCRIPT_DIR}/tmp/tuner-measurement" "${RESULTS_DIRECTORY}/dbt2-data/"
-mv "${SCRIPT_DIR}/tmp/tuner-final" "${RESULTS_DIRECTORY}/dbt2-data/"
-rm -rf "${SCRIPT_DIR}/tmp"
 # Copy infrastructure.yml and vars.yml
 cp "${SCRIPT_DIR}/../infrastructure.yml" "${SCRIPT_DIR}/dbt2-data/"
 cp "${SCRIPT_DIR}/../vars.yml" "${RESULTS_DIRECTORY}/dbt2-data/"
