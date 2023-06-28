41a0a9851588126e03970a6a80daed2612e75185
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-28 04:08:51 +0000

    aws-tpcc-refdata - handle benchmark results within the playbook

--- aws-tpcc-refdata/04_execute/run.sh
+++ aws-tpcc-refdata/04_execute/run.sh
@@ -7,7 +7,7 @@ export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
 TERRAFORM_PROJECT_PATH="${RESULTS_DIRECTORY}/${TERRAFORM_PROJECT_NAME}"
 
-# Run the benchmark with refdata enabled
+# Run the benchmark with refdata enabled and disabled
 ansible-playbook \
 	-u ${SSH_USER} \
 	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
@@ -18,30 +18,9 @@ ansible-playbook \
 	-e "tpcc_warehouse=${TPCC_WAREHOUSE}" \
 	-e "tpcc_vusers=${TPCC_VUSERS}" \
 	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
-	-e "use_refdata=1" \
+	-e "results_directory=${RESULTS_DIRECTORY}/report-data" \
 	${SCRIPT_DIR}/playbook-tpcc-run.yml
 
-# Run the benchmark withou refdata
-ansible-playbook \
-	-u ${SSH_USER} \
-	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
-	-i ${SCRIPT_DIR}/../inventory.yml \
-	-e "@${SCRIPT_DIR}/../vars.yml" \
-	-e "tpcc_duration=${TPCC_DURATION}" \
-	-e "tpcc_rampup=${TPCC_RAMPUP}" \
-	-e "tpcc_warehouse=${TPCC_WAREHOUSE}" \
-	-e "tpcc_vusers=${TPCC_VUSERS}" \
-	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
-	-e "use_refdata=0" \
-	${SCRIPT_DIR}/playbook-tpcc-run.yml
-
-# Generate charts
-python3 ${SCRIPT_DIR}/post-processing.py
-
-# Move data to results directory
-[[ ! -d "$RESULTS_DIRECTORY/report-data" ]] && mkdir -p "$RESULTS_DIRECTORY/report-data"
-# Copy collected data and generated data & charts
-cp -r ${SCRIPT_DIR}/benchmark_data ${RESULTS_DIRECTORY}/report-data
 # Copy infrastructure.yml and vars.yml
 cp "../infrastructure.yml" "$RESULTS_DIRECTORY"
 cp "../vars.yml" "$RESULTS_DIRECTORY"
