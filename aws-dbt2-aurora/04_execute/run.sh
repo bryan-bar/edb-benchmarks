ff2f192122567d57ff04caba4b2b4d738c228b23
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-06-28 04:04:50 +0000

    aws-dbt2-aurora - handle benchmark results within the playbook

--- aws-dbt2-aurora/04_execute/run.sh
+++ aws-dbt2-aurora/04_execute/run.sh
@@ -15,15 +15,9 @@ ansible-playbook \
 	-e "dbt2_warehouse=${DBT2_WAREHOUSE}" \
 	-e "dbt2_connections=${DBT2_CONNECTIONS}" \
 	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
+	-e "results_directory=${RESULTS_DIRECTORY}/dbt2-data" \
 	./playbook-dbt2-run.yml
 
-# Move data to results directory
-[[ ! -d "$RESULTS_DIRECTORY/dbt2-data" ]] && mkdir -p "$RESULTS_DIRECTORY/dbt2-data"
-mv ./dbt2_data/* "$RESULTS_DIRECTORY/dbt2-data"
-# Extract the archive containing the report and data
-tar xzf "$RESULTS_DIRECTORY/dbt2-data/dbt2-data.tar.gz" -C .
-mv ./tmp/dbt2-data/* "$RESULTS_DIRECTORY/dbt2-data"
-rm -rf ./tmp
 # Copy infrastructure.yml and vars.yml
 cp ../infrastructure.yml "$RESULTS_DIRECTORY/dbt2-data"
 cp ../vars.yml "$RESULTS_DIRECTORY/dbt2-data"
